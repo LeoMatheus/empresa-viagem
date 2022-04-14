614f0a4015ecc3b23bfc9e4af6f4ba16ad6595b9
Leo Matheus <leomraifur@gmail.com>
2022-04-14 09:17:43 -0300

    Sim

--- js/script-reserva.js
+++ js/script-reserva.js
@@ -8,12 +8,12 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 const closeModal = function () {
   modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  // overlay.classList.add('hidden');
 };
 
 const openModal = function () {
   modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+  // overlay.classList.remove('hidden');
 };
 
 
