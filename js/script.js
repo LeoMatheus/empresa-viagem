5b4b597713c196781ffd96eaa043d8ada2437aef
Leo Matheus <leomraifur@gmail.com>
2022-04-16 08:39:10 -0300

    Commit nav

--- js/script.js
+++ js/script.js
@@ -3,27 +3,31 @@ var i = 0;
 
 let header = document.querySelector('.main-head');
 
-setTimeout(() => {
-    if(confirm("Gostaria de receber promoções e pacotes de viagem?")){
-        let email = prompt("Nos informe seu email");
+(function(){
+    alert("Seja bem vindo ao nosso site");
+})();
+
+// setTimeout(() => {
+//     if(confirm("Gostaria de receber promoções e pacotes de viagem?")){
+//         let email = prompt("Nos informe seu email");
     
-        if(email !== null){
-            alert("Email cadastrado com sucesso");
-        } else{
-            alert("Campo Vazio")
-        }
+//         if(email !== null){
+//             alert("Email cadastrado com sucesso");
+//         } else{
+//             alert("Campo Vazio")
+//         }
         
-    }
-}, 1000);
+//     }
+// }, 1000);
 
 
 
 
-let mudarCorHeader = function(){
-    let cor = ["#6A9CD9", "#B0C5DF", "#474F59", "#5177A6"];
+// let mudarCorHeader = function(){
+//     let cor = ["#6A9CD9", "#B0C5DF", "#474F59", "#5177A6"];
 
-    header.style.backgroundColor = cor[i];
-    i = (i + 1) % cor.length;
-}
+//     header.style.backgroundColor = cor[i];
+//     i = (i + 1) % cor.length;
+// }
 
-setInterval(mudarCorHeader, 2000);
+// setInterval(mudarCorHeader, 2000);
